7c8e1f60fe14df2d4976ee965ce5429152e98e8e
piltirmon86 <fernandogomezrecio@gmail.com>
2023-08-29 12:02:55 +0200

    version 1.7

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../contexts/auth.context'
 import logoDark from './../../assets/images/logoDark.png'
@@ -13,51 +13,55 @@ const Navigation = () => {
 
         <Navbar bg="dark" data-bs-theme="dark">
 
-            <Navbar.Brand className='ms-5' ><Link to={'/'} className='nav-link'><img
-                alt=""
-                src={logoDark}
-                width="100"
-                height="100"
-                className="d-inline-block align-top"
-            /></Link></Navbar.Brand>
+            <Container>
+                <Navbar.Brand className='ms-10' ><Link to={'/'} className='nav-link'><img
+                    alt=""
+                    src={logoDark}
+                    width="100"
+                    height="100"
+                    className="d-inline-block align-top"
+                /></Link></Navbar.Brand>
 
-            <Nav className="me-auto">
+                <Nav className="me-auto">
 
-                <Link to={'/events'} className='nav-link'> Eventos </Link>
-
-                {
-                    loggedUser &&
-
-                    <Link to={'/user/community'} className='nav-link'> Comunidad </Link>
-
-                }
-
-            </Nav>
-
-            <Nav className='justify-content-end'>
-
-                <NavDropdown id="basic-nav-dropdown" title="Icono carita">
-                    {/* <img src={iconProfile} alt="" /> */}
-                    {
-                        !loggedUser &&
-                        <>
-                            <Link to={'/signup'} className='nav-link'> Crear Registro </Link>
-                            <Link to={'/login'} className='nav-link'> Iniciar Sesión </Link>
-                        </>
-                    }
-
+                    <Link to={'/events'} className='nav-link'> Eventos </Link>
 
                     {
                         loggedUser &&
-                        <>
-                            <Link to={'/user/profile'} className='nav-link'> Perfil de usuario </Link>
-                            <Link className='nav-link' onClick={logout}>Cerrar sesión</Link>
-                        </>
+
+                        <Link to={'/user/community'} className='nav-link'> Comunidad </Link>
+
                     }
-                </NavDropdown>
 
-            </Nav>
+                </Nav>
 
+                <Nav className='justify-content-end'>
+
+                    <NavDropdown id="basic-nav-dropdown" className='userIcon'
+                    // title={<div className="userIcon"
+                    //     style={{ width: "50px", height: "50px", borderRadius: "50%" }}>
+                    // </div>}
+                    >
+
+                        {
+                            !loggedUser &&
+                            <>
+                                <Link to={'/signup'} className='nav-link'> Crear Registro </Link>
+                                <Link to={'/login'} className='nav-link'> Iniciar Sesión </Link>
+                            </>
+                        }
+
+
+                        {
+                            loggedUser &&
+                            <>
+                                <Link to={'/user/profile'} className='nav-link'> Mi perfil </Link>
+                                <Link className='nav-link' onClick={logout}>Cerrar sesión</Link>
+                            </>
+                        }
+                    </NavDropdown>
+                </Nav>
+            </Container>
         </Navbar >
     )
 }
