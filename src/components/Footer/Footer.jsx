e7acc8be661ac64854cf4caea6dacdd6ad8f0a69
Piltrimon86 <fernandogomezrecio@gmail.com>
2023-09-07 09:29:45 +0200

    version 4.7

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,22 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/theme.context'
 import './Footer.css'
 
-
-const Footer = () => <footer >Todos los derechos reservados</footer>
+const Footer = ({ message }) => {
+    const { theme } = useContext(ThemeContext); 
+  
+    const footerStyle = {
+      backgroundColor: theme === 'dark' ? '#fff' : '#333',
+      color: theme === 'dark' ? '#333' : '#fff',
+      border: 'solid 2px black'
+    };
+  
+    return (
+      <footer style={footerStyle}>
+        {message || 'Todos los derechos reservados'}
+      </footer>
+    )
+  }
 
 
 export default Footer
