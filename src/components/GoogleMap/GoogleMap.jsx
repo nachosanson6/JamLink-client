92602d563b6d1781d273a002f6b612f79e8cec24
nacho <nachosanson6@gmail.com>
2023-09-07 09:28:08 +0200

    version 4.6 empezando con los comentarios

--- src/components/GoogleMap/GoogleMap.jsx
+++ src/components/GoogleMap/GoogleMap.jsx
@@ -1,98 +1,80 @@
 import React, { useState } from 'react'
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import SpinnerComponent from '../Spinner/Spinner';
-
-
-const containerStyle = {
-    // width: '440px',
-    height: '500px'
-};
+import './marker.css'
+import marker from './../../assets/images/marker.png'
 
 
 
+const Mapper = ({ location, label }) => {
 
-const Mapper = (location, label) => {
+    const containerStyle = {
+        // width: '440px',
+        height: '500px'
+    };
+
+    const markerCenter = location.map(elm => {
+        return { lat: elm.coordinates[1], lng: elm.coordinates[0] }
+    })
+
+    const markerLabel = label.map(elm => {
+        return elm
+    })
+
+
+
+
+
+
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: "AIzaSyCIkt_MWj32EbnKrxghvdDSFRzxDfC4uMs"
     })
 
-    console.log('tamañoooooooo', location.length)
-
-    if (location.length > 1) {
-        const center = {
-            lat: location.location[0].location.coordinates[1],
-            lng: location.location[0].location.coordinates[0]
-        }
-        const [map, setMap] = React.useState(null)
-        const [markerPosition, setMarkerPosition] = useState(center)
-
-        const onLoad = React.useCallback(function callback(map) {
-            const bounds = new window.google.maps.LatLngBounds(center);
-            map.fitBounds(bounds);
-
-            setMap(map)
-        }, [center])
-
-        const onUnmount = React.useCallback(function callback(map) {
-            setMap(null)
-        }, [])
-
-        return isLoaded ? (
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={15}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-
-            >
-                <Marker position={markerPosition} label={location.label} />
 
 
-                <></>
-            </GoogleMap>
-        ) : <></>
-
-    } else {
-        console.log('trtrtrtr', location.location[0].coordinates[1])
-        const center = {
-            lat: location.location[0].coordinates[1],
-            lng: location.location[0].coordinates[0]
-        }
-
-        const [map, setMap] = React.useState(null)
-        const [markerPosition, setMarkerPosition] = useState(center)
-
-        const onLoad = React.useCallback(function callback(map) {
-            const bounds = new window.google.maps.LatLngBounds(center);
-            map.fitBounds(bounds);
-
-            setMap(map)
-        }, [center])
-
-        const onUnmount = React.useCallback(function callback(map) {
-            setMap(null)
-        }, [])
-        if (!location) {
-            return <SpinnerComponent />
-        }
-        return isLoaded ? (
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={15}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-
-            >
-                <Marker position={markerPosition} label={location.label} />
-
-
-                <></>
-            </GoogleMap>
-        ) : <></>
+    if (!location) {
+        return <SpinnerComponent />
     }
+
+    const center = {
+        lat: location[0].coordinates[1],
+        lng: location[0].coordinates[0]
+    }
+
+    const [map, setMap] = React.useState(null)
+    const [markerPosition, setMarkerPosition] = useState(center)
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+
+
+
+        >
+            {markerCenter.map((eachMarker, idx) => {
+                return (
+                    <Marker key={idx}
+                        position={eachMarker}
+                        icon={{ url: marker, scaledSize: new window.google.maps.Size(70, 55) }}
+                        label={{
+                            text: markerLabel[idx],
+                            className: 'marker-label',
+                            pixelOffset: new window.google.maps.Size(-15, -15) // Ajusta el desplazamiento para alinear el centro
+                        }}
+                    />
+
+                )
+            })}
+
+
+            <></>
+        </GoogleMap>
+    ) : <></>
+
+
 }
 
 export default Mapper
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react"
 import { Form, Button } from "react-bootstrap"
 import authService from "../../services/auth.services"
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from './../../contexts/auth.context'
 
 
 const LoginForm = ({ fireFinalActions }) => {
